361d5725a8cfa00462a38b7a9551f6b3135ba550
dacasfilipe <filiperdacas@gmail.com>
2025-06-18 14:48:14 -0300

    update app title to 'Spring - Angular App' and change property declaration

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,8 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'angularclient';
+  title: string;
+  constructor(){
+    this.title = 'Spring - Angular App';
+  }
 }
